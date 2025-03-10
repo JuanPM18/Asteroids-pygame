23389aa30ed340c6091d6aad84093102aa4ed27a
JuanPM18 <jpm.018@gmail.com>
2025-03-10 11:51:40 -0400

    Ch2:Lesson3

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, (0,0,0)) # written by me
         # pygame.Surface.fill(screen, color = "black") # written by me
         # screen.fill("black") # from boot.dev solution
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = fps.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
